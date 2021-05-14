9977a9003f86eee7647db4606c659028d0506585
Guidance.Educare <84131410+Guidance-Educare@users.noreply.github.com>
2021-05-14 23:51:15 +0530

    Add files via upload

--- Script/uploadFiles.js
+++ Script/uploadFiles.js
@@ -2,6 +2,7 @@
 var uploader = document.getElementById("uploader");
 var fileButton = document.getElementById("fileButton");
 var path = "";
+var imgUrl = "";
 function Science() {
   document.getElementById("Science").checked = true;
   document.getElementById("SSt").checked = false;
@@ -24,7 +25,9 @@ function Maths() {
 //Listen for file selection
 fileButton.addEventListener("change", function (e) {
   if (path !== "") {
-    
+
+    document.getElementById("error").innerHTML =" "
+
     //Get the file
     var file = e.target.files[0];
 
@@ -38,15 +41,29 @@ fileButton.addEventListener("change", function (e) {
     //Update prgress bar
     task.on(
       "state_changed",
-
-      function progress(snapshort) {
+      function (snapshot) {
         var percentage =
-          (snapshort.bytesTransferred / snapshort.totalBytes) * 100;
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         uploader.value = percentage;
       },
 
       function error(err) {
-        console.log(err);
+        alert("Error in Uploding The img");
+      },
+
+      function () {
+        task.snapshot.ref.getDownloadURL().then(function(url) {
+          imgUrl = url;
+
+          File_Name = file.name.split('.')
+          finalFile = File_Name[0]
+
+          firebase.database().ref(path + finalFile).set({
+              Name: file.name,
+              Link: imgUrl,
+            });
+            document.getElementById("error").innerHTML ="File Uploaded Successfully";
+        });
       },
 
       function complete() {}
