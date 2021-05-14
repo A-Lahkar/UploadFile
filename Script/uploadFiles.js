2e7001e00ccf6d695ca00ad1a5feeae26e8109d1
Guidance.Educare <84131410+Guidance-Educare@users.noreply.github.com>
2021-05-14 21:18:37 +0530

    Add files via upload

--- Script/uploadFiles.js
+++ Script/uploadFiles.js
@@ -1,48 +1,56 @@
 //get elements
 var uploader = document.getElementById("uploader");
 var fileButton = document.getElementById("fileButton");
-
+var path = "";
 function Science() {
   document.getElementById("Science").checked = true;
   document.getElementById("SSt").checked = false;
   document.getElementById("Maths").checked = false;
+  path = "Science/";
 }
 function SSt() {
   document.getElementById("Science").checked = false;
   document.getElementById("SSt").checked = true;
   document.getElementById("Maths").checked = false;
+  path = "SSt/";
 }
 function Maths() {
   document.getElementById("Science").checked = false;
   document.getElementById("SSt").checked = false;
   document.getElementById("Maths").checked = true;
+  path = "maths/";
 }
 
 //Listen for file selection
 fileButton.addEventListener("change", function (e) {
-  //Get the file
-  var file = e.target.files[0];
+  if (path !== "") {
 
-  //Create a storage ref
-  var storageRef = firebase.storage().ref("sst/" + file.name); //own names for folder and file
+    //Get the file
+    var file = e.target.files[0];
 
-  //Upload file
-  var task = storageRef.put(file);
+    //Create a storage ref
+    var storageRef = firebase.storage().ref(path + file.name); //own names for folder and file
 
-  //Update prgress bar
-  task.on(
-    "state_changed",
+    //Upload file
+    var task = storageRef.put(file);
 
-    function progress(snapshort) {
-      var percentage =
-        (snapshort.bytesTransferred / snapshort.totalBytes) * 100;
-      uploader.value = percentage;
-    },
+    //Update prgress bar
+    task.on(
+      "state_changed",
 
-    function error(err) {
-      console.log(err);
-    },
+      function progress(snapshort) {
+        var percentage =
+          (snapshort.bytesTransferred / snapshort.totalBytes) * 100;
+        uploader.value = percentage;
+      },
 
-    function complete() {}
-  );
+      function error(err) {
+        console.log(err);
+      },
+
+      function complete() {}
+    );
+  } else {
+    document.getElementById("error").innerHTML = "Please Enter the Subject";
+  }
 });
